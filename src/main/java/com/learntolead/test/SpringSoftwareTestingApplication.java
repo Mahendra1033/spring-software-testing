376159a175f0d0181ab80913fbe389e4f4bc77e2
Mahendra1033 <mahendramahiee1033@gmail.com>
2022-05-22 00:02:51 +0530

    Project Initial Commit

---
+++ src/main/java/com/learntolead/test/SpringSoftwareTestingApplication.java
@@ -0,0 +1,13 @@
+package com.learntolead.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSoftwareTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSoftwareTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learntolead/test/SpringSoftwareTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learntolead.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSoftwareTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
